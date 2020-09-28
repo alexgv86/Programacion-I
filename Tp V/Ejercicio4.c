e2acd8800b36e81607870ec9805638c11f873ca9
Alejandro Gonzalez Videla <gv.alejandro86@gmail.com>
2020-09-27 21:54:59 -0300

    Se agregó condicional al punto 4 para que sólo admita números

--- Tp V/Ejercicio4.c
+++ Tp V/Ejercicio4.c
@@ -12,34 +12,43 @@ errores por favor reportelos en el foro (http://pseint.sourceforge.net). */
    otra variable con scanf. */
 
 int main() {
-	float contador,i,mayor,menor,numero,promedio;
+	float comprobar,contador,i,mayor,menor,numero,promedio;
 	float total=0;
 	printf("En esta ocasion, el ejercicio se divide en 3 partes:\n");
 	printf("Primero ingresamos la cantidad de numeros que vamos a querer ingresar.\n");
 	printf("Segundo, iremos ingresando los numeros respectivos.\n");
 	printf("Por ultimo, el programa devolvera la sumatoria total, el promedio, tanto como el numero mayor y el menor ingresado.\n");
 	printf("Ingrese la cantidad de numeros que desea utilizar: \n");
-	scanf("%f",&contador);
-	for (i=1;i<=contador;i+=1) {
-		printf("Ingrese un numero: \n");
-		scanf("%f",&numero);
-		total = total+numero;
-		if (i==1) {
-			mayor = numero;
-			menor = numero;
-		} else {
-			if (numero>mayor) {
-				mayor = numero;
-			} else {
-				if (numero<menor) {
+	comprobar = scanf("%f",&contador) == 1 ? 1 : 0;
+	if(comprobar == 1){
+		for (i=1;i<=contador;i+=1) {
+			printf("Ingrese un numero: \n");
+			comprobar = scanf("%f",&numero) == 1 ? 1 : 0;
+			if(comprobar ==1){
+				total = total+numero;
+				if (i==1) {
+					mayor = numero;
 					menor = numero;
+				} else {
+					if (numero>mayor) {
+						mayor = numero;
+					} else {
+						if (numero<menor) {
+							menor = numero;
+						}
+					}
 				}
+			}else{
+				printf("Solo se admiten numeros!");
+				return 0;
 			}
 		}
-	}
-	promedio = total/contador;
-	printf("El total es: %f y el promedio es: %f\n",total,promedio);
-	printf("El valor mas pequeno ingresado es: %f y el mas grande ingresado es: %f\n",menor,mayor);
-	return 0;
+		promedio = total/contador;
+		printf("El total es: %f y el promedio es: %f\n",total,promedio);
+		printf("El valor mas pequeno ingresado es: %f y el mas grande ingresado es: %f\n",menor,mayor);
+		return 0;
+		}else{
+			printf("Solo se admiten numeros!");
+			return 0;}
 }
 
