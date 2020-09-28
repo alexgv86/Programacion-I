377365cada4014e77c7a96e32efa5629c5f06ddf
Alejandro Gonzalez Videla <gv.alejandro86@gmail.com>
2020-09-27 23:39:34 -0300

    Se corrigió el ejercicio 7 del TP V

--- Tp V/Ejercicio7.c
+++ Tp V/Ejercicio7.c
@@ -7,12 +7,11 @@ int main(){
 	printf("Ingresaremos cuantas ventas tuvo cada una de las 10 sucursales\n");
 	for(i=0;i<10;i++){
 		printf("Ingrese las ventas para la sucursal %d\n",i+1);
-		do{
 			comprobar = (scanf("%d",&sucursal[i]) == 1) ? 1 : 0;
 			if(comprobar == 0){
 				printf("Solo se admiten numeros enteros!\n");
+				return 0;
 			}
-		}while(comprobar==0);
 	}
 	for(i=0;i<10;i++){
 		printf("La sucursal %d tuvo %d ventas\n",i+1,sucursal[i]);
